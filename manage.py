3604aca5d652b881a81b45425c1698f33aff26f1
Atnhai <atnhai@gmail.com>
2022-06-06 16:38:48 +0700

    .

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django3_PersonalPortfolio.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_thawanrat_resume.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
